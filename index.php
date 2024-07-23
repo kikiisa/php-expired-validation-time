b01034b43ed98c34d4d2926ffc2168aa04c06d1c
kikiisa <kikiisa89@gmail.com>
2024-07-23 20:27:18 +0800

    fix

--- index.php
+++ index.php
@@ -4,60 +4,54 @@ require "vendor/autoload.php";
 use Carbon\Carbon;
 use Carbon\Exceptions\InvalidFormatException;
 
-
-class phpValidationTime
+function repairDate($date)
 {
-
-    function repairDate($date)
+    if(strpos($date,"/"))
     {
-        if(strpos($date,"/"))
-        {
-            $part = explode("/",$date);
-            $transormation = $part[2] . "-" . $part[1] . "-" . $part[0];
-            return $transormation;
-        }else{
-            return $date;
-        }
-    }
-    function convertToYMD($dateString, $dateFormat = null) {
-        try {
-            // Convert date to Carbon instance with given format or default to parse
-            if ($dateFormat) {
-                $date = Carbon::createFromFormat($dateFormat, $dateString);
-            } else {
-                $date = Carbon::parse($dateString);
-            }
-            
-            // Return the date in 'Y-m-d' format
-            return $date->format('Y-m-d');
-        } catch (InvalidFormatException $e) {
-            return "Format tanggal tidak valid: " . $dateString;
-        } catch (Exception $e) {
-            return "Terjadi kesalahan: " . $e->getMessage();
-        }
-    }
-    function isExpired($issueDate, $validityYears) {
-        // check 
-        // Convert issue date to Carbon instance
-        $issueDate = Carbon::parse($issueDate);
-        // Get the current date
-        $currentDate = Carbon::now();
-        // Calculate the expiration date (validityYears years after issue date)
-        $expirationDate = $issueDate->addYears($validityYears);
-        // Check if current date is after expiration date
-        if($currentDate->greaterThan($expirationDate))
-        {
-            return "Masa Berlaku Sudah Habis";
-        }else{
-            return "Masa Berlaku Masih Aktif";
-        }
-    
+        $part = explode("/",$date);
+        $transormation = $part[2] . "-" . $part[1] . "-" . $part[0];
+        return $transormation;
+    }else{
+        return $date;
     }
 }
+function convertToYMD($dateString, $dateFormat = null) {
+    try {
+        // Convert date to Carbon instance with given format or default to parse
+        if ($dateFormat) {
+            $date = Carbon::createFromFormat($dateFormat, $dateString);
+        } else {
+            $date = Carbon::parse($dateString);
+        }
+        
+        // Return the date in 'Y-m-d' format
+        return $date->format('Y-m-d');
+    } catch (InvalidFormatException $e) {
+        return "Format tanggal tidak valid: " . $dateString;
+    } catch (Exception $e) {
+        return "Terjadi kesalahan: " . $e->getMessage();
+    }
+}
+function isExpired($issueDate, $validityYears) {
+    // check 
+    // Convert issue date to Carbon instance
+    $issueDate = Carbon::parse($issueDate);
+    // Get the current date
+    $currentDate = Carbon::now();
+    // Calculate the expiration date (validityYears years after issue date)
+    $expirationDate = $issueDate->addYears($validityYears);
+    // Check if current date is after expiration date
+    if($currentDate->greaterThan($expirationDate))
+    {
+        return "Masa Berlaku Sudah Habis";
+    }else{
+        return "Masa Berlaku Masih Aktif";
+    }
 
+}
 
-// $issueDate = '1999-07-12';
-// $validityYears = 5;
+$issueDate = '1999-07-12';
+$validityYears = 5;
 // echo convertToYMD($issueDate,"Y-m-d");
 // echo repairDate($issueDate);
-// isExpired(repairDate($issueDate), $validityYears);
+echo isExpired(repairDate($issueDate), $validityYears);
